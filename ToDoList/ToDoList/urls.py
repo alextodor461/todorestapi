4632fa6c04d796b283e7354418ac7f1365d03f98
AlexTodor <alex_todor@yahoo.com>
2022-08-04 13:53:37 +0200

    test

--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -4,10 +4,9 @@ from todo.views import ToDoViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'todos/', ToDoViewSet)
+router.register(r'todos', ToDoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('', include('rest_framework.urls', namespace='rest_framework'))
 ]
