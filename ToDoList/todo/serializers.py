a21ed7b29623914dbfd60658bf5f870b0d43d73e
AlexTodor <alex_todor@yahoo.com>
2022-08-04 16:56:16 +0200

    user Model added

--- ToDoList/todo/serializers.py
+++ ToDoList/todo/serializers.py
@@ -2,7 +2,16 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import ToDo
 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name']
+
+
 class ToDoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = ToDo
-        fields = ['id', 'title', 'description', 'created_at'] #, 'user'
+        fields = ['id', 'title', 'description', 'created_at', 'user'] #, 'user'
