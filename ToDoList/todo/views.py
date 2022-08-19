a0be6fd8db7e087f89940c9f8946432479d2a749
AlexTodor <alex_todor@yahoo.com>
2022-08-19 09:26:51 +0200

    test

--- ToDoList/todo/views.py
+++ ToDoList/todo/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
-from todo.serializers import ToDoSerializer
+from django.core import serializers
 from .models import ToDo
 from .serializers import ToDoSerializer
+from django.http import HttpResponse
 
 
 class ToDoViewSet(viewsets.ModelViewSet):
@@ -13,4 +14,9 @@ class ToDoViewSet(viewsets.ModelViewSet):
     """
     queryset = ToDo.objects.all().order_by('-created_at')
     serializer_class = ToDoSerializer
-    #permission_classes = [] #permissions.IsAuthenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    def Create(self, request):
+        todo = ToDo.objects.create(title=request.POST.get('title', ''), description=request.POST.get('description', ''), user=request.user)
+        serializer_obj = serializer.serialize('json', [todo, ])
+        return HttpResponse(serializer_obj)
